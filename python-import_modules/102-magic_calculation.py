a45437dd0f67bd9ebb4d3bd0a33d708d3250efb9
vuwase <v.uwase@alustudent.com>
2023-07-05 10:32:20 -0700

    102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -5,10 +5,9 @@ def magic_calculation(a, b):
 
     if a < b:
        c = add(a, b)
-       
-       for i in range(4, 6):
+        for i in range(4, 6):
             c = add(c, i)
         return (c)
-
-    else:
+   
+      else:
         return(sub(a, b))
