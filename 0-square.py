7dec25304d45a00ee424b3c5cc9e8372787b52f8
vuwase <v.uwase@alustudent.com>
2023-07-26 12:52:21 -0700

    0-square.py

--- 0-square.py
+++ 0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+""" Write an empty class Square that defines a square
+"""
+
+
+class Square:
+    """ An empty class that defines a square"""
+    pass
