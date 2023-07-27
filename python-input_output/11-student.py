af21bea631a1146d11f27056cb35fbae6ee98f3d
vuwase <v.uwase@alustudent.com>
2023-07-27 13:36:42 -0700

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""class_to_json"""
 
 
 class Student:
-    """Represent a student."""
+    """Contains student data
+    """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = Kevin
-        self.last_name = Mc Joe
-        self.age = 45
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """class_to_json"""
+
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """reload_from_json"""
+
+        for items in json.keys():
+            self.__dict__[items] = json[items]
