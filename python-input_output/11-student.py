270ba1c0053256b7204683667caa8a0cb8698ef8
vuwase <v.uwase@alustudent.com>
2023-07-27 13:33:36 -0700

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,6 +12,6 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        self.first_name = Kevin
+        self.last_name = Mc Joe
+        self.age = 45
