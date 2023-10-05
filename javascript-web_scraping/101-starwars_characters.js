d44397ae1a8343c7caf7084b7a2f4dfb52b59f8d
vuwase <v.uwase@alustudent.com>
2023-10-05 09:58:15 -0700

    100-starwars_characters.js 101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,11 +1,22 @@
 #!/usr/bin/node
+/*
 const request = require('request');
-request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
-  if (err) console.log(err);
-  logCharacter(JSON.parse(body).characters, 0);
-});
+const args = process.argv;
+let dicList = {};
 
-function logCharacter (characters, i) {
-  if (i > characters.length - 1) {
-    return;
-  }
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const charEnd = data.characters;
+    const loopTime = charEnd.length;
+    for (let i = 0; i < loopTime; i++) {
