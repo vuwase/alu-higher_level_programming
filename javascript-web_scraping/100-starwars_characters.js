dbe9c7c356b7f36ed0a6366d835f6ff6b4c29f22
vuwase <v.uwase@alustudent.com>
2023-10-05 09:50:59 -0700

    100-starwars_characters.js 101-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,45 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`An error occurred while making the request: ${error}`);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-      return;
-    }
-
-    try {
-      const movie = JSON.parse(body);
-      const charactersUrls = movie.characters;
-
-      charactersUrls.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(`An error occurred while making the request: ${error}`);
-            return;
-          }
-
-          if (response.statusCode !== 200) {
-            console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-            return;
-          }
-
-          const character = JSON.parse(body);
-          console.log(character.name);
-        });
-      });
-    } catch (error) {
-      console.error(`An error occurred while parsing the response: ${error}`);
-    }
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) console.log(err);
+  JSON.parse(body).characters.forEach(el => {
+    request.get(el, (error, res, data) => {
+      if (error) console.log(err);
+      console.log(JSON.parse(data).name);
+    });
   });
-} else {
-  console.log('Please provide the Movie ID as an argument.');
-}
+});
