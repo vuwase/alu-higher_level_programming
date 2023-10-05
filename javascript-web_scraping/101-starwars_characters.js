6fced8cb54ad037071336e1a3967de0e33ba1e21
vuwase <v.uwase@alustudent.com>
2023-10-05 10:06:04 -0700

    updated

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,50 @@
 #!/usr/bin/node
-/*
+
 const request = require('request');
-const args = process.argv;
-let dicList = {};
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+  request.get(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error(`An error occurred while making the request: ${error}`);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+      return;
+    }
+
+    try {
+      const movie = JSON.parse(body);
+      const charactersUrls = movie.characters;
+
+      const printCharacters = (urls, index = 0) => {
+        if (index >= urls.length) {
+          return;
+        }
+
+        request.get(urls[index], (error, response, body) => {
+          if (error) {
+            console.error(`An error occurred while making the request: ${error}`);
+          } else if (response.statusCode !== 200) {
+            console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+          } else {
+            const character = JSON.parse(body);
+            console.log(character.name);
+          }
+
+          printCharacters(urls, index + 1);
+        });
+      };
+
+      printCharacters(charactersUrls);
+    } catch (error) {
+      console.error(`An error occurred while parsing the response: ${error}`);
+    }
+  });
+} else {
+  console.log('Please provide the Movie ID as an argument.');
 }
-
-request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    const charEnd = data.characters;
-    const loopTime = charEnd.length;
-    for (let i = 0; i < loopTime; i++) {
